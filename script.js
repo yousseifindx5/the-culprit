ca1834e64562f24f5be0c805c237a8b132621b39
yousseifindx5 <yousseifindx5@gmail.com>
2026-05-20 18:29:52 +0300

    started styling the game page

---
+++ script.js
@@ -0,0 +1,10 @@
+const Start_Btn = document.getElementById("startBtn");
+const Help_Btn = document.getElementById("helpBtn");
+
+Start_Btn.addEventListener("click", function () {
+    alert("game setup will be added next.");
+});
+
+Help_Btn.addEventListener("click", function () {
+    alert("instruction screen coming soon.");
+});
