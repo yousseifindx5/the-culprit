15610b309b927792fb1f367a18d80d03d933c221
yousseifindx5 <yousseifindx5@gmail.com>
2026-05-20 18:11:16 +0300

    Created first game interface

---
+++ script.js
@@ -0,0 +1,10 @@
+const Start_Btn = document.getElementById("startBtn");
+const Help_Btn = document.getElementById("helpBtn");
+
+Start_Btn.onclick = function () {
+    alert("game setup will be added next.");
+}
+
+Help_Btn.onclick = function () {
+    alert("instruction screen coming soon.");
+}
